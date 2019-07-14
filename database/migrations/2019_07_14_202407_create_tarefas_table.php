276ae153ca6aeef6e42eac5dc7e7128b8bce1b58
Rodrigo <rodrigosoares8899@gmail.com>
2019-07-14 17:34:27 -0300

    Commit Inicial

---
+++ database/migrations/2019_07_14_202407_create_tarefas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('descricao', 200);
+            $table->string('usuario', 75);
+            $table->date('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
